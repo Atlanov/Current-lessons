71484486da3a57a518110a69a3ec383170fb3885
Atlanov <atlanov@mail.ru>
2024-07-04 11:37:51 +0300

    Add files via upload

---
+++ Homework1.py
@@ -0,0 +1,7 @@
+print('Strings and string indexing')
+example = 'Northumberland'
+print(example[0])
+print(example[-1])
+print(example[7:])
+print(example[::-1])
+print(example[0::2])
---
+++ Homework2.py
@@ -0,0 +1,5 @@
+completed_homeworks = 12
+sum_of_spent_hours = 1.5
+title_of_course = 'Python'
+time_for_one_task = sum_of_spent_hours / completed_homeworks
+print('Курс:',title_of_course,', всего задач:',completed_homeworks,', затрачено часов:',sum_of_spent_hours,', среднее время выполнения:',time_for_one_task,'часа')
