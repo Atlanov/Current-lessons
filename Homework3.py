32ac7795b1bece3a8528a9687515c9533fcbb3c5
Atlanov <atlanov@mail.ru>
2024-07-04 14:49:20 +0300

    Add files via upload

---
+++ Homework3.py
@@ -0,0 +1,9 @@
+print('Practical work')
+name = 'Aleksandr'
+print('Name:', name)
+age = 54
+print('Age:', age)
+age = age+1
+print('Newage:', age)
+is_student = True
+print('Is student:', is_student)
---
+++ Homework4.py
@@ -0,0 +1,8 @@
+print('Организация программ и методы строк')
+my_string = input('Введите Вашу строку: ')
+print((len(my_string)), 'знаков и символов')
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
